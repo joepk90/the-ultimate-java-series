fe891560b7579c3c48c3955382a1ec0ffb25cac7
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2023-05-17 17:03:03 +0100

    P1: 2.05 - primative vs reference types

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,10 +1,12 @@
 import java.util.Date;
+import java.awt.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
         // HelloWorld.variables();
         // HelloWorld.primativeTypes();
-        HelloWorld.referenceTypes();
+        // HelloWorld.referenceTypes();
+        HelloWorld.referenceVsPrimativeTypes();
     }
 
     public static void variables() {
@@ -26,5 +28,17 @@ public class HelloWorld {
         Date now = new Date();
         // now.getTime()
         System.out.println(now); // shortcut: sout
+    }
+
+    public static void referenceVsPrimativeTypes() {
+        byte x = 1;
+        byte y = x;
+        x = 2;
+        System.out.println(y); // prints 1, because y is a primative type and set to 1 when we reference x
+
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2; // this will update the value of x of the Point reference object also assigned to the point1 variable
+        System.out.println(point2); // point 2 x will also equal 2
     }
 }
