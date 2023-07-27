4d358f35c28575449f475d9baaf469c53c91b6de
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2023-07-27 20:21:41 +0100

    2.8 - arrays

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,5 +1,6 @@
 import java.util.Date;
 import java.awt.*;
+import java.util.Arrays;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -7,7 +8,8 @@ public class HelloWorld {
         // HelloWorld.primativeTypes();
         // HelloWorld.referenceTypes();
         // HelloWorld.referenceVsPrimativeTypes();
-        HelloWorld.strings();
+        // HelloWorld.strings();
+        HelloWorld.arrays();
     }
 
     public static void variables() {
@@ -64,4 +66,20 @@ public class HelloWorld {
         System.out.println(escapedNewLine);
         System.out.println(escapedTab);
     }
+
+    public static void arrays() {
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+
+        System.out.println(Arrays.toString(numbers));
+
+        // java arrays have a fixed length - 5
+        int[] numbers2 = { 2, 3, 5, 1, 4 };
+        System.out.println(numbers.length);
+
+        Arrays.sort(numbers2);
+        System.out.println(Arrays.toString(numbers2));
+    }
+
 }
