c3aaf424b5965d7a1b408d9f7ca37fdd29feeca6
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-06-15 15:55:33 +0100

    P1: 3.07 - convert if statement in switchStatements method to switch statement

--- HelloWorld/src/ControlFlow.java
+++ HelloWorld/src/ControlFlow.java
@@ -60,12 +60,17 @@ public class ControlFlow {
 
     public static void switchStatements() {
         String role = "admin";
-        if (role == "admin")
-            System.out.println("You're an admin");
-        else if (role == "moderator")
-            System.out.println("You're a moderator");
-        else
-            System.out.println("You're a guest");
-            
+
+        switch (role) {
+            case "admin":
+                System.out.println("You're an admin");
+                break;
+            case "moderator":
+                System.out.println("You're a moderator");
+                break;
+            default:
+                System.out.println("You're a guest");
+        }
+
     }
 }
