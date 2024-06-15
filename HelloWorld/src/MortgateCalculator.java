4d96afcfe37a7f8cf5d1c965299a4f1f35ad7d28
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-06-15 20:36:28 +0100

    P1: 3.14 - update MortgateCalculator logic - use while loops to improve data validation of user input

--- HelloWorld/src/MortgateCalculator.java
+++ HelloWorld/src/MortgateCalculator.java
@@ -8,15 +8,61 @@ public class MortgateCalculator {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principle: ");
-        int principle = scanner.nextInt();
+         /**
+         * Principle (Loan)
+         */
+        int principle = 0;
+        while(true) {
+            
+            System.out.print("Principle ($1k - $1M): ");
+            principle = scanner.nextInt();
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
+            if (principle >= 1000 && principle <= 1_000_000) {
+                break;
+            }
+
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
+                
+        }
+        
+        /**
+         * Annual Interest Rate
+         */
+        float annualInterest = 0.0f;
+        while(true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+
+            if (annualInterest > 0.0f && annualInterest < 30.0f) {
+                break;
+            }
+
+            System.out.println("Enter a value greater than 0 and less than or equal to 30");
+                
+        }
+
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
 
-        System.out.print("Period (Years): ");
-        byte period = scanner.nextByte();
+        /**
+         * Period
+         */
+        byte period = 0;
+        while(true) {
+            
+            System.out.print("Period (Years): ");
+            scanner.nextByte();
+
+            if (period > 0 && period <= 30) {
+                break;
+            }
+
+            System.out.println("Enter a value between 1 and 30");
+                
+        }
+
+        /**
+         * Calculations
+         */
         int numberOfPayments = period * MONTHS_IN_YEAR;
 
         double mortage = principle * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
