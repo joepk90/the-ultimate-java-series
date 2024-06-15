fa0b44b8217168db62a6c4a93170c9d47f492377
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-06-15 14:34:02 +0100

    P1: 3.02 - add comparison operators method in ControlFlow class

--- HelloWorld/src/ControlFlow.java
+++ HelloWorld/src/ControlFlow.java
@@ -1,5 +1,17 @@
 public class ControlFlow {
     
-    public static void main(String[] args) { }
+    public static void main(String[] args) {
+        ControlFlow.comparisonOperators();
+    }
 
+    public static void comparisonOperators() {
+        int x = 1;
+        int y = 1;
+        System.out.println(x == y); // true
+        System.out.println(x != y); // false
+        System.out.println(x > y); // false
+        System.out.println(x >= y); // true
+        System.out.println(x < y); // false
+        System.out.println(x <= y); // true
+    }
 }
