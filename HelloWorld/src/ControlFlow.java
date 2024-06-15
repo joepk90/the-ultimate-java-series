a5ee813bb4cb298af96e0243cba601f4b507d5aa
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-06-15 14:44:30 +0100

    P1: 3.04 - add ifStatements method to ControlFlow class - document if statement logic

--- HelloWorld/src/ControlFlow.java
+++ HelloWorld/src/ControlFlow.java
@@ -2,7 +2,8 @@ public class ControlFlow {
     
     public static void main(String[] args) {
         // ControlFlow.comparisonOperators();
-        ControlFlow.logicalOperators();
+        // ControlFlow.logicalOperators();
+        ControlFlow.ifStatements();
     }
 
     public static void comparisonOperators() {
@@ -26,5 +27,23 @@ public class ControlFlow {
         boolean hasCriminalRecord = false;
         boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
         System.out.println(isEligible); // true
+    }
+
+    public static void ifStatements() {
+        int temp = 32;
+
+        if (temp > 30) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink water");
+        } else if (temp > 20 & temp <= 30) {
+            System.out.println("Beautiful day");
+        } else {
+            System.out.println("Cold day");
+        }
+           
+        if (true)
+            // curley braces are only required if there are multiple statements
+            System.out.println("Cold day");
+
     }
 }
