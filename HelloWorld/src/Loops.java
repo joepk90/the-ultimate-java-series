d8d03ac7346b062a673ad0643b2039e7bd04c6ac
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-06-15 16:28:14 +0100

    P1: 3.09 - add forLoop method in Loops class - setup basic for loop examples

--- HelloWorld/src/Loops.java
+++ HelloWorld/src/Loops.java
@@ -1,5 +1,14 @@
 public class Loops {
     static public void main(String[] args) {
+        Loops.forLoops();
+    }
 
+    public static void forLoops() {
+        for (int i = 0; i < 5; i++)
+            System.out.println("Hello World " + i);
+
+        for (int i = 5; i > 0; i--) {
+            System.out.println("Hello World " + i);
+        }   
     }
 }
