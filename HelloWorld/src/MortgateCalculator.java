8872cc79105e5ab80263ff98bd16e35878ae221e
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-06-15 22:15:38 +0100

    P1: 4.05 - extract main calculation logic in MortgateCalculator class to a new method calledcalculateMortgate

--- HelloWorld/src/MortgateCalculator.java
+++ HelloWorld/src/MortgateCalculator.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 
 public class MortgateCalculator {
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+        
+        
 
         int principle = 0;
-        float monthlyInterest = 0;
-        
-        int numberOfPayments = 0;
+        float annualInterest = 0;
+        byte years = 0;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -31,15 +30,13 @@ public class MortgateCalculator {
         /**
          * Annual Interest Rate
          */
-        float annualInterest = 0.0f;
         while(true) {
             System.out.print("Annual Interest Rate: ");
             annualInterest = scanner.nextFloat();
 
-            if (annualInterest > 0.0f && annualInterest < 30.0f) {
-                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+            if (annualInterest > 0.0f && annualInterest < 30.0f)
                 break;
-            }
+        
 
             System.out.println("Enter a value greater than 0 and less than or equal to 30");
         }
@@ -49,26 +46,37 @@ public class MortgateCalculator {
          */
         while(true) {
             System.out.print("Period (Years): ");
-            byte years = scanner.nextByte();
+            years = scanner.nextByte();
 
-            if (years > 1 && years <= 30) {
-                numberOfPayments = years * MONTHS_IN_YEAR;
+            if (years > 1 && years <= 30)
                 break;
-            }
 
             System.out.println("Enter a value between 1 and 30"); 
         }
 
-        /**
-         * Calculations
-         */
-        double mortage = principle * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
-                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-
+       
+        double mortage = calculateMortgate(principle, annualInterest, years);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortage);
         System.out.println("Mortgage: " + mortgageFormatted);
 
         scanner.close();
+    }
+
+    public static double calculateMortgate(
+            int principle,
+            float annualInterest,
+            byte years
+        ) {
+            final byte MONTHS_IN_YEAR = 12;
+            final byte PERCENT = 100;
+            float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+            short numberOfPayments = (short)(years * MONTHS_IN_YEAR);
+            
+
+            double mortage = principle * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+            / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+            return mortage;
     }
 
 }
