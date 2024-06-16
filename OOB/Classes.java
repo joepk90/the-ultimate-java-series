2194977a552967f7b4b2e1c0035d56c6a0811f04
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-06-16 21:10:58 +0100

    P2: 2.05 - setup memoryAllocation documenting the heap and stack, and how varaibles and objects are stored and referenced

--- OOB/Classes.java
+++ OOB/Classes.java
@@ -1,6 +1,7 @@
 public class Classes {
     public static void main(String[] args) {
-        creatingObjects();
+        // creatingObjects();
+        memoryAllocation();
     }
 
     public static void creatingObjects() {
@@ -11,5 +12,36 @@ public class Classes {
         var textBox2 = new TextBox();
         textBox2.setText("Box 2");
         System.out.println(textBox1.text);
+    }
+
+    public static void memoryAllocation() {
+         /**
+         * Heap: stores objects
+         */
+
+         /**
+         * Stack: stores short lived variables
+         * - primatives
+         * - varialbes whcih stores references ot objects on the heap
+         */
+
+        var textBox1 = new TextBox();
+
+        /**
+         * TextBox object:
+         * - Storied on the heap
+         * 
+         * 
+         * Variable:
+         * - Allocated memory on the stack.
+         * - In this memory location it will store the address of the TextBox obbject on the heap
+         */
+
+        var textBox2 = textBox1;
+        // both textBox1 and textBox2 are referencing the same TextBox object on the heap.
+        // the two varaible on the stack which reference the same TextBox object
+
+        textBox2.setText("Hello World");        
+        System.out.println(textBox2.text); // "Hello World
     }
 }
