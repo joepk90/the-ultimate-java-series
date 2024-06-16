ac0fba2a8f68664d7ffed77296a2edfcb3db08b6
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-06-16 20:59:06 +0100

    P2: 2.04 - organise code - move textbox code to new creatingObjects method

--- OOB/Classes.java
+++ OOB/Classes.java
@@ -1,5 +1,9 @@
 public class Classes {
     public static void main(String[] args) {
+        creatingObjects();
+    }
+
+    public static void creatingObjects() {
         var textBox1 = new TextBox();
         textBox1.setText("Box 1");
         System.out.println(textBox1.text.toUpperCase());
