58b1c60a914c0144c7142b1e30d44fabb4a002f7
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-06-16 21:56:34 +0100

    P2: 2.08 - setup getter and setter methods for Employee baseSalery property. update Employee usage in OOB main.java file

--- OOB/Employee.java
+++ OOB/Employee.java
@@ -1,8 +1,19 @@
 public class Employee {
-    public int baseSalery;
+    private int baseSalery;
     public int hourlyRate;
 
     public int calculateWage(int extraHours) {
         return baseSalery + (hourlyRate & extraHours);
+    }
+
+    public void setBaseSalary(int baseSalery) {
+        if (baseSalery <= 0)
+            throw new IllegalArgumentException("Base Salary cannot be 0 or less");
+
+        this.baseSalery = baseSalery;
+    }
+
+    public int getBaseSalary() {
+        return baseSalery;
     }
 }
--- OOB/Main.java
+++ OOB/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
         public static void main(String[] args) {
             var employee = new Employee();
-            employee.baseSalery = 50_000;
+            employee.setBaseSalary(50_000);
             employee.hourlyRate = 20;
 
             int wage = employee.calculateWage(10);
