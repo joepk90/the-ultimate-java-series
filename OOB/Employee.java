4fbeaf35d279e7b0436aa0dc5be6922bef965228
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-06-16 21:58:43 +0100

    P2: 2.08 - setup getter and setter methods for Employee hourlyRate property. update Employee usage in OOB main.java file

--- OOB/Employee.java
+++ OOB/Employee.java
@@ -1,6 +1,6 @@
 public class Employee {
     private int baseSalery;
-    public int hourlyRate;
+    private int hourlyRate;
 
     public int calculateWage(int extraHours) {
         return baseSalery + (hourlyRate & extraHours);
@@ -15,5 +15,16 @@ public class Employee {
 
     public int getBaseSalary() {
         return baseSalery;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly Rate cannot be 0 or negative");
+
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
     }
 }
--- OOB/Main.java
+++ OOB/Main.java
@@ -3,7 +3,7 @@ public class Main {
         public static void main(String[] args) {
             var employee = new Employee();
             employee.setBaseSalary(50_000);
-            employee.hourlyRate = 20;
+            employee.setHourlyRate(20);
 
             int wage = employee.calculateWage(10);
             
