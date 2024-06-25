5608b6da9517c78f97bc71e53adcba216deb2912
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-06-25 20:25:32 +0100

    P2: 4.05 - document inherited private members - private fields or methods inherited from parent classes

--- OOB/TextBox.java
+++ OOB/TextBox.java
@@ -2,6 +2,7 @@ public class TextBox extends UIControl {
 
     public TextBox() {
         super(true); // super must be called as the first statement at the top of the contructor
+        // this.isEnabled // inherited fields (private members) are not accessible in sub classes
         System.out.println("TextBox");
     }
 
