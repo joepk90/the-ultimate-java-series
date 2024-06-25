de60930fe2977b9273ebd7f34c355b744d77ae94
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-06-25 20:33:47 +0100

    P2: 4.05 - document the protected access modifier in the UIControl class

--- OOB/UIControl.java
+++ OOB/UIControl.java
@@ -1,4 +1,15 @@
 public class UIControl {
+    
+    /**
+     * Protected Access Modifiers
+     * - protected makes the field public, but only within the same package (package com.ultimatejavaseries;)
+     * - declaring a field or method as protected, makes it acceissble to
+     * instances of the UIControl object within this package
+     * - the protected access modifier is considered bad practice
+     */
+
+    // protected boolean isEnabled = true;
+
     private boolean isEnabled = true;
 
     public UIControl(boolean isEnabled) {
