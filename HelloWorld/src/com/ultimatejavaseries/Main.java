eeb675a6128c829f5146791f97e8b52f661ab942
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-07-20 14:52:49 +0100

    P2: 5.06 - move MortgageCalculator logic in HelloWorld class main method to new static method: runMortgageCalculator

--- HelloWorld/src/com/ultimatejavaseries/Main.java
+++ HelloWorld/src/com/ultimatejavaseries/Main.java
@@ -6,13 +6,17 @@ import MortgageProject.MortgageReport;
 
 public class Main {
     public static void main(String[] args) {
+        Main.runMortgageCalculator();
+    }
+
+    public static void runMortgageCalculator() {
         int principle = (int) Console.readNumber("Principle ($1k - $1M): ", 1000, 1_000_000);
         float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
         byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
         var calculator = new MortgageCalculator(principle, annualInterest, years);
-        var report =  new MortgageReport(calculator);
+        var report = new MortgageReport(calculator);
         report.printMortgage();
-        report.printPaymentSchedule(); 
+        report.printPaymentSchedule();
     }
 }
