65568cc47f9dfb632f19522a3828e2ed5d744d4f
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-07-20 15:39:28 +0100

    P2: 5.10 - add example MyTube project files

---
+++ MyTube/VideoEncoder.java
@@ -0,0 +1,8 @@
+package MyTube;
+
+public class VideoEncoder {
+    public void encode(Video video) {
+        System.out.println("Encoding video...");
+        System.out.println("Done!\n");
+    }
+}
