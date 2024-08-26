a825ad9ff62c308f4aaa302b794b9f92129bab72
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-08-26 17:17:26 +0100

    P3: 2.06 - setup example catch statement which handles two types of exceptions: IOException and ParseException

--- Code/Code/src/com/theultimatejavaseries/advanced/exceptions/ExceptionsDemo.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/exceptions/ExceptionsDemo.java
@@ -3,6 +3,8 @@ package com.theultimatejavaseries.advanced.exceptions;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
@@ -15,15 +17,16 @@ public class ExceptionsDemo {
         try {
             var reader = new FileReader("file.txt");
             var value = reader.read();
+            new SimpleDateFormat().parse("");
         } catch (FileNotFoundException e) {
-            // e.printStackTrace();
+            // // e.printStackTrace();
             System.out.println("File does not exist.");
-
-            // because IOException is more generic, it must come after the
-            // FileNotFoundException catch block
-            // otherwise the FileNotFoundException catch block can never be reached
-        } catch (IOException e) {
-            IOException ex = new FileNotFoundException();
+        }
+        // because IOException is more gener
+        // FileNotFoundException catch block
+        // otherwise the FileNotFoundException catch block can never be reached
+        catch (IOException | ParseException e) {
+            // IOException ex = new FileNotFoundException();
             System.out.println("Could not read data.");
         }
 
