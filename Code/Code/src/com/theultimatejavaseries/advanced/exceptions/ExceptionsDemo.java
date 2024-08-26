f04aff33fcca8cf645fbc8df20f0d37f04dd5ca4
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-08-26 16:17:44 +0100

    P3: 2.03 - add example of a checked exception using FileReader class. add comment to existing unchecked exception

--- Code/Code/src/com/theultimatejavaseries/advanced/exceptions/ExceptionsDemo.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/exceptions/ExceptionsDemo.java
@@ -1,11 +1,23 @@
 package com.theultimatejavaseries.advanced.exceptions;
 
+// import java.io.FileReader;
+
 public class ExceptionsDemo {
     public static void show() {
+
+        // example of unchecked exception
         sayHello(null);
     }
 
     public static void sayHello(String name) {
         System.out.println(name.toUpperCase());
     }
+
+    // example of checked exception
+    // public static void checkedException() {
+    // FileNotFoundExceptionJava:
+    // example of checked exception - an error which is
+    // checked at compile time
+    // var reader = new FileReader("file.txt");
+    // }
 }
