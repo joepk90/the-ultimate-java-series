a6f1805846c6ea1e28614a0cd7002710f81d3c49
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-08-29 20:43:11 +0100

    P3: 3.03 - setup poor mans approach to making the List class reusable for different types. update the List type to an object so any value can be accepted in the list. demosntrate usage in the generics Main class

--- Code/Code/src/com/theultimatejavaseries/advanced/generics/Main.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/generics/Main.java
@@ -3,6 +3,16 @@ package com.theultimatejavaseries.advanced.generics;
 public class Main {
     public static void main(String[] args) {
         var list = new List();
-        list.add(1);
+        list.add(1); // java compiler will convert this value to Integer.valueOf(1)
+        list.add("1");
+        list.add(new User());
+
+        int number = (int) list.get(0);
+        // this causes an error because the second
+        // item added is a string ("1")
+        // int number = (int) list.get(1);
+
+        // using the Object type for our List, means we are unable to catch type casting
+        // bugs like this at compile time.
     }
 }
