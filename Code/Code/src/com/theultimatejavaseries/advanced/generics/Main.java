5145e88e0148f21fc2a69b163686e291f26629d2
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-08-29 20:27:20 +0100

    P3: 3.02 - create new package generics. create new List class. instantiate and use the List class in the advanced Main class

---
+++ Code/Code/src/com/theultimatejavaseries/advanced/generics/Main.java
@@ -0,0 +1,8 @@
+package com.theultimatejavaseries.advanced.generics;
+
+public class Main {
+    public static void main(String[] args) {
+        var list = new List();
+        list.add(1);
+    }
+}
