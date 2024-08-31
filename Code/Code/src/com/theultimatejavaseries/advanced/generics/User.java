c2a5af38e697931b834ccc68f8c2d9d747d153e6
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-08-31 12:50:15 +0100

    P3: 3.08 - document the Comparable Interface - update the User class to implement the Comparable interface. add points property to User class + override the Comparable interface compareTo method.

--- Code/Code/src/com/theultimatejavaseries/advanced/generics/User.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/generics/User.java
@@ -1,5 +1,26 @@
 package com.theultimatejavaseries.advanced.generics;
 
-public class User implements Comparable {
+public class User implements Comparable<User> {
+    private int points;
 
+    // default contructor added to prevent compilition errors of existing usages
+    public User() {
+        this.points = 0;
+    }
+
+    public User(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public int compareTo(User other) {
+        // if (points < other.points)
+        // return -1;
+        // if (points == other.points)
+        // return 0;
+        // return -1;
+
+        // same expression as above
+        return points - other.points;
+    }
 }
