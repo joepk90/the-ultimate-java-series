d6c14dcc88e39500b34ac1f2d6badc96a81d6b26
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-08-31 16:03:47 +0100

    P3: 4.03 - the need for iterables - demonstrate issue of GenericList class - being unable to iterate through the GenericList classes items

--- Code/Code/src/com/theultimatejavaseries/advanced/collections/Main.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/collections/Main.java
@@ -1,7 +1,20 @@
 package com.theultimatejavaseries.advanced.collections;
 
+import com.theultimatejavaseries.advanced.generics.GenericList;
+
 public class Main {
     public static void main(String[] args) {
+        theNeedForIterables();
+    }
 
+    public static void theNeedForIterables() {
+        var list = new GenericList<String>();
+        list.add("a");
+        list.add("b");
+
+        // unable to iterate over the list without exposing the items property on the
+        // GenericList class
+        // for (var item : list.items)
+        // System.out.println(item);
     }
 }
