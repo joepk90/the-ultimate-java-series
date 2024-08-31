0eca57409d4ff653db17f9887eb10933ac0a699d
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-08-31 16:16:28 +0100

    P3: 4.04 - iterable interface - update the GenericList class to implement the Iterable interface

--- Code/Code/src/com/theultimatejavaseries/advanced/generics/GenericList.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/generics/GenericList.java
@@ -1,6 +1,8 @@
 package com.theultimatejavaseries.advanced.generics;
 
-public class GenericList<T> {
+import java.util.Iterator;
+
+public class GenericList<T> implements Iterable<T> {
     private T[] items = (T[]) new Object[10];
     private int count;
 
@@ -10,6 +12,11 @@ public class GenericList<T> {
 
     public T get(int index) {
         return items[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
     }
 }
 
