4a42f6566837bc066e174caa9b5ee12524f5eefb
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-09-01 16:16:32 +0100

    P3: 4.11 - the set interface - add further exampls of the Set Interface using the HashSet class. document common operations: Union, Intersection and Difference

--- Code/Code/src/com/theultimatejavaseries/advanced/collections/SetDemo.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/collections/SetDemo.java
@@ -1,7 +1,12 @@
 package com.theultimatejavaseries.advanced.collections;
 
 import java.util.HashSet;
+// import java.util.List;
 import java.util.Set;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SetDemo {
     public static void show() {
@@ -13,6 +18,29 @@ public class SetDemo {
         set.add("blue");
 
         System.out.println(set); // returns: [sky, blue, is]
+
+        Collection<String> collection = new ArrayList<String>();
+        Collections.addAll(collection, "a", "b", "c", "c");
+        Set<String> collectionSet = new HashSet<String>(collection);
+
+        // passing a Collection with duplicates to a Set, will remove duplicates
+        System.out.println(collectionSet); // returns: [a, b, c]
+
+        // List<String> list = new ArrayList<String>() - alternatively use Arrays.asList
+        Set<String> set1 = new HashSet<String>(Arrays.asList("a", "b", "c"));
+        Set<String> set2 = new HashSet<String>(Arrays.asList("b", "c", "d"));
+
+        // Union: combination of two sets (two sets without duplicates)
+        // set1.addAll(set2);
+        // System.out.println(set1); // returns: [a, b, c, d]
+
+        // Intersection: returns items which are common across multiple sets
+        // set1.retainAll(set2);
+        // System.out.println(set1); // returns: [b, c]
+
+        // Difference: returns items which don't exist in the second set
+        set1.removeAll(set2);
+        System.out.println(set1); // returns: [b, c]
 
     }
 }
