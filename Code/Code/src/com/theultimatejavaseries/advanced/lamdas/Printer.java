6292475e0aa7fa95bfa8a1504e119b49080dd466
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-09-02 08:43:19 +0100

    P3: 5.02 - functional interfaces - add example absract and default method to the Printer class + add documentation

--- Code/Code/src/com/theultimatejavaseries/advanced/lamdas/Printer.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/lamdas/Printer.java
@@ -1,5 +1,13 @@
 package com.theultimatejavaseries.advanced.lamdas;
 
 public interface Printer {
+    // abstract method
+    void print(String message);
 
+    // default method (negative java feature)
+    // interfaces should not have implementation - they are contracts
+    // default void printTwice(String message) {
+    // System.out.println(message);
+    // System.out.println(message);
+    // }
 }
