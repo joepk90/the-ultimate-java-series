99ad2346fcedf06924846b465c092da1e43b6b42
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-10-05 15:16:42 +0100

    P3: 5.08 - consumer interface - add example consumer interface links to documention + setup example Consumer interface usage using a List of ints with the forEach method

--- Code/Code/src/com/theultimatejavaseries/advanced/lamdas/FunctionalInterfaces.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/lamdas/FunctionalInterfaces.java
@@ -1,5 +1,7 @@
 package com.theultimatejavaseries.advanced.lamdas;
 
+import java.util.List;
+
 public class FunctionalInterfaces {
 
     /**
@@ -17,7 +19,28 @@ public class FunctionalInterfaces {
      * The Consumer interface represents an operation that takes a single argument
      * and returns no result.
      * This is why is it is called a consumer, because it consumes a value
+     * 
+     * Example Consumer Interfaces:
+     * https://docs.oracle.com/javase/8/docs/api/?java/util/function/package-summary.html
+     * https://docs.oracle.com/javase/8/docs/api/java/util/function/BiConsumer.html
+     * https://docs.oracle.com/javase/8/docs/api/java/util/function/IntConsumer.html
+     * https://docs.oracle.com/javase/8/docs/api/java/util/function/DoubleConsumer.html
+     * https://docs.oracle.com/javase/8/docs/api/java/util/function/LongConsumer.html
      */
+
+    public static void consumerInterfaces() {
+        List<Integer> intList = List.of(1, 2, 3);
+
+        // Imperative Programming (for, if/else, switch/case):
+        // how should be done...
+        for (var item : intList)
+            System.out.println(item);
+
+        // Declarative Programming:
+        // what should be done...
+        intList.forEach(item -> System.out.println(item)); // the forEach method requires a Consumer
+        // the forEach method consumes an argument, but does not return the value
+    }
 
     /**
      * Suppliers:
