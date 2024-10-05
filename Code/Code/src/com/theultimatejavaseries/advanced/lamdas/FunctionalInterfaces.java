c5a5b8ac63e62af82d0f9d6d2148e7670175db6a
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-10-05 14:01:25 +0100

    P3: 5.07 - built-in functional interfaces - add new functional interfaces class in lamdas directory - add documention around the different types of built-in unctional interfaces in java

---
+++ Code/Code/src/com/theultimatejavaseries/advanced/lamdas/FunctionalInterfaces.java
@@ -0,0 +1,41 @@
+package com.theultimatejavaseries.advanced.lamdas;
+
+public class FunctionalInterfaces {
+
+    /**
+     * Built-In Functional Interfaces
+     * https://docs.oracle.com/javase/8/docs/api/?java/util/function/package-summary.html
+     * 
+     * - Consumers
+     * - Suppliers
+     * - Functions
+     * - Predicates
+     */
+
+    /**
+     * Consumers:
+     * The Consumer interface represents an operation that takes a single argument
+     * and returns no result.
+     * This is why is it is called a consumer, because it consumes a value
+     */
+
+    /**
+     * Suppliers:
+     * The Opposite of the Consumer interface. It represents an operation that takes
+     * no input
+     * and returns a value. It supplies a value.
+     */
+
+    /**
+     * Functions:
+     * The Function interface represents a function that can map a value to a
+     * different value.
+     */
+
+    /**
+     * Predicates:
+     * The Predicate interface represents an operation that takes an object,
+     * and checks to see if that object satisfies some criteria.
+     */
+
+}
