a5bf1be22e5f4f014dc99ace614f19e045e7ae22
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-10-13 21:00:43 +0100

    P3: 6.02 - imperative vs functional programming - setup Movie class. setup imperative and declarative programming examples using Movie class in StreamsDemo class + add documentation. use stream functionality to demostrate declarative programming

--- Code/Code/src/com/theultimatejavaseries/advanced/streams/StreamsDemo.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/streams/StreamsDemo.java
@@ -1,5 +1,36 @@
 package com.theultimatejavaseries.advanced.streams;
 
+import java.util.List;
+
+/**
+ * Streams:
+ * - a stream is a sequence of objects, but unlike a collection, s it doesn't
+ * store data. it is a way of getting data out of a collection.
+ * - streams were introduced in Java to allow us to process a collection of
+ * objects in a declarative (or more accurately functional) way
+ * - functional programming is a special type of declarative programming but
+ * with additional concepts
+ */
+
 public class StreamsDemo {
+    public static void imperativeOrFunctional() {
+        List<Movie> movies = List.of(
+                new Movie("a", 10),
+                new Movie("b", 15),
+                new Movie("c", 20));
+
+        // Imperative Programming:
+        // - statements that specify how something should be done
+        int count = 0;
+        for (var movie : movies)
+            if (movie.getLikes() > 10)
+                count++;
+
+        // Declarative Programming:
+        // - statements that specify how something should be done
+        var count2 = movies.stream() // every collection in Java has a stream method
+                .filter(movie -> movie.getLikes() > 10);
+
+    }
 
 }
