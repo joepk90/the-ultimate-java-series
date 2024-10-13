fdc9cf099c9e4ca4db1a6b0a79dcfcf24f013628
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-10-13 20:41:18 +0100

    P3: 6.02 - imperative vs functional programming - setup a streams package with Main and StreamsDemo class

---
+++ Code/Code/src/com/theultimatejavaseries/advanced/streams/Main.java
@@ -0,0 +1,7 @@
+package com.theultimatejavaseries.advanced.streams;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Code/Code/src/com/theultimatejavaseries/advanced/streams/StreamsDemo.java
@@ -0,0 +1,5 @@
+package com.theultimatejavaseries.advanced.streams;
+
+public class StreamsDemo {
+
+}
