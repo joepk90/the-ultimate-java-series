037a79f3b3fdc1239582d21103b9be5896bd03ef
joepk90 <joeparkkennaby@gmail.com>
2024-10-19 09:25:31 +0100

    P3: 7.02 - processes and threads - use static methods thread.activeCount and runtime.getRuntime to find out the number of active threads in the current process and the total number of available threads

--- Code/Code/src/com/theultimatejavaseries/advanced/concurrency/ConcurrencyDemo.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/concurrency/ConcurrencyDemo.java
@@ -1,5 +1,11 @@
 package com.theultimatejavaseries.advanced.concurrency;
 
 public class ConcurrencyDemo {
-    
+    public static void processesAndThreads() {
+        // returns number of active threads in the current process
+        System.out.println(Thread.activeCount());
+
+        // returns total number of available threads
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
 }
--- Code/Code/src/com/theultimatejavaseries/advanced/concurrency/Main.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/concurrency/Main.java
@@ -2,5 +2,6 @@ package com.theultimatejavaseries.advanced.concurrency;
 
 public class Main {
     public static void main(String[] args) {
+        ConcurrencyDemo.processesAndThreads();
     }
 }
