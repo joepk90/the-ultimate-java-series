0e78a22c2cdc39b812371796617472270823639f
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-10-20 19:21:39 +0100

    P3: 7.13 - the volatile keyword - update the DownloadStatusWithVolatileSync class to use the volatile keyword on the isDone field, instead of the synchronized keyword on the done and isDone methods

--- Code/Code/src/com/theultimatejavaseries/advanced/concurrency/DownloadStatusWithVolatileSync.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/concurrency/DownloadStatusWithVolatileSync.java
@@ -1,7 +1,18 @@
 package com.theultimatejavaseries.advanced.concurrency;
 
+/**
+ * Volatile Keyword
+ * Another tool for writing thread safe code, but without the overhead of
+ * synchronization - the volatile keyword.
+ * 
+ * The volatile keyword solves the visibility problem, but not the race
+ * condition. It doesn't prevent two threads from simultaneously modifying data,
+ * instead it ensures that if one thread changes data, other threads can see the
+ * changes.
+ */
+
 public class DownloadStatusWithVolatileSync {
-    private Boolean isDone = false;
+    private volatile Boolean isDone = false; // volatile means unstable
     private int totalBytes;
     private Object totalBytesLock = new Object();
 
@@ -18,12 +29,24 @@ public class DownloadStatusWithVolatileSync {
         }
     }
 
-    public synchronized Boolean isDone() {
+    public Boolean isDone() {
         return isDone;
     }
 
-    public synchronized void done() {
+    public void done() {
         isDone = true;
     }
+
+    // Synchronized Keyword -> Volatile Keyword:
+    // instead of using the synchronized keyword,
+    // the volatile keyword is applied to the isDone field
+
+    // public synchronized Boolean isDone() {
+    // return isDone;
+    // }
+
+    // public synchronized void done() {
+    // isDone = true;
+    // }
 
 }
