b325c6d95f40a19d6675565df025ac48cc1bbd12
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2024-10-20 20:44:02 +0100

    P3: 7.16 - adders - setup a adders method in the ConcurrencyDemo class. create new adders download status class - solve race condition problems using adders

--- Code/Code/src/com/theultimatejavaseries/advanced/concurrency/ConcurrencyDemo.java
+++ Code/Code/src/com/theultimatejavaseries/advanced/concurrency/ConcurrencyDemo.java
@@ -290,4 +290,25 @@ public class ConcurrencyDemo {
 
         System.out.println(status.getTotalBytes());
     }
+
+    public static void adders() {
+        DownloadStatusInterface status = new DownloadStatusWithAdder();
+
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new DownloadFileTaskWithStatusArg(status));
+            thread.start();
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(status.getTotalBytes());
+    }
 }
---
+++ Code/Code/src/com/theultimatejavaseries/advanced/concurrency/DownloadStatusWithAdder.java
@@ -0,0 +1,28 @@
+package com.theultimatejavaseries.advanced.concurrency;
+
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * Adder Classes:
+ * internally, the (LongAdder) Adder object keeps an array of counters
+ * that can grow on demand. so we don't have a single place in memory
+ * where our values are stored, instead we have a set of array cells that
+ * each hold a counter value.
+ * 
+ * different threads can modify these counter varaibles concurrently, which is
+ * why Adder classes are faster than Atomic types, because they allow more
+ * throughput.
+ */
+
+public class DownloadStatusWithAdder implements DownloadStatusInterface {
+    private LongAdder totalBytes = new LongAdder();
+
+    public int getTotalBytes() {
+        return totalBytes.intValue();
+        // LongAdder intValue calls the sum method, which adds up all the counter values
+    }
+
+    public void incrementTotalBytes() {
+        totalBytes.increment();
+    }
+}
